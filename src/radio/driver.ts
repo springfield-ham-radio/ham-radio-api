f2edd06938ce839b1a883f8135f8b37318cea39f
Bryan Hunt <bhunt@nvidia.com>
2023-03-03 21:52:19 -0600

    Allow importFromRadio() to return undefined when cancelled.

--- src/radio/driver.ts
+++ src/radio/driver.ts
@@ -2,6 +2,6 @@ import { RadioProgram } from './program.js';
 import { RadioProgressIndicator } from './progress-indicator.js';
 
 export interface RadioDriver<Settings> {
-  importFromRadio(path: string, progressIndicator: RadioProgressIndicator): Promise<RadioProgram<Settings>>;
+  importFromRadio(path: string, progressIndicator: RadioProgressIndicator): Promise<RadioProgram<Settings>> | undefined;
   programRadio(path: string, program: RadioProgram<Settings>, progressIndicator: RadioProgressIndicator): Promise<void>;
 }
