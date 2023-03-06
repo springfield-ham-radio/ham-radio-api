ef0c11ad99e12896d201e8fd125c0a0458e6d7e7
Bryan Hunt <bhunt@nvidia.com>
2023-03-05 19:42:52 -0600

    Move module API to driver module

--- src/radio/module.ts
+++
@@ -1,7 +0,0 @@
-import { RadioDriver } from './driver.js';
-import { RadioManufacturer } from './manufacturer.js';
-
-export interface RadioModule {
-  getManufacturer(): RadioManufacturer;
-  getDriver(modelName: string): RadioDriver;
-}
