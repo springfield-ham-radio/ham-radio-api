6ede16a9876b91420db58b08476ca424b21ca30a
Bryan Hunt <bhunt@nvidia.com>
2025-04-05 23:18:53 -0500

    feat!: remove radio id from RadioProgram

--- src/radio/program.ts
+++ src/radio/program.ts
@@ -1,9 +1,7 @@
-import { RadioId } from '../branded-types/radio-id.js';
 import { RadioProgrammedChannel } from './programmed-channel.js';
 import { RadioSettings } from './settings.js';
 
 export interface RadioProgram {
-  radio: RadioId;
   settings: RadioSettings;
   channels: RadioProgrammedChannel[];
 }
