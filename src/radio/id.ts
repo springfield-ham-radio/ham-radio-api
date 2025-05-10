a7bb04036977141bf5d79c8434c6190b2f7d61c4
Bryan Hunt <bhunt@nvidia.com>
2025-05-10 11:04:47 -0500

    docs: add documentation

--- src/radio/id.ts
+++ src/radio/id.ts
@@ -1,6 +1,12 @@
 import { RadioModelId } from '../branded-types/radio-model-id.js';
 import { RadioModuleId } from '../branded-types/radio-module-id.js';
 
+/**
+ * A unique identifier for a radio.
+ *
+ * This information is passed between the renderer process and Electron process to identify a radio.
+ * It must be serializable to JSON.
+ */
 export interface RadioId {
   module: RadioModuleId;
   model: RadioModelId;
