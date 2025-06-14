30d026acb12a7cadab2cfc09dc55c050101c9289
Bryan Hunt <bhunt@mac.com>
2025-06-13 22:26:53 -0500

    feat!: update RadioMemory interface to use Uint8Array for contents
    - Changed contents property in RadioMemory from Uint8Array[] to Uint8Array for improved memory management.

--- src/radio/memory.ts
+++ src/radio/memory.ts
@@ -2,5 +2,5 @@ import type { RadioModelId } from '../branded-types/radio-model-id.js';
 
 export interface RadioMemory {
   radioModel: RadioModelId;
-  contents: Uint8Array[];
+  contents: Uint8Array;
 }
