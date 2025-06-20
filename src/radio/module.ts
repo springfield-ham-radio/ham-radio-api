96ac2d38e41855640f01c3e184b7a2cd6de2100c
Bryan Hunt <bhunt@mac.com>
2025-06-19 21:34:02 -0500

    fix: update import path for Radio type in module.ts
    - Changed import statement for Radio type from './radio-old.js' to './radio.js' to reflect the new file structure.

--- src/radio/module.ts
+++ src/radio/module.ts
@@ -1,4 +1,4 @@
-import type { Radio } from './radio-old.js';
+import type { Radio } from './radio.js';
 import type { RadioModuleId } from '../branded-types/radio-module-id.js';
 
 export interface RadioModule {
