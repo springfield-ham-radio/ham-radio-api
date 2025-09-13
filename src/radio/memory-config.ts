88aa089148681a9a8cac4442716278222dd9ddf0
Bryan Hunt <bhunt@mac.com>
2025-09-13 09:26:58 -0500

    feat: add addressEndianness property to RadioMemoryConfig interface

--- src/radio/memory-config.ts
+++ src/radio/memory-config.ts
@@ -3,6 +3,7 @@ import type { RadioMemorySegment } from './memory-segment.js';
 export interface RadioMemoryConfig {
   chunkSize: number;
   addressSize: number;
+  addressEndianness: 'big' | 'little';
   segments: {
     [segmentName: string]: RadioMemorySegment;
   };
